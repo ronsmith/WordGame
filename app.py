6d58690f17640f69452284c2a62e94ce4a6c8fc1
ron <ron.smith@thataintworking.com>
2024-02-05 22:11:59 -0500

    created empty templates

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
 from flask import Flask, render_template
+import sqlite3
 
 app = Flask('WordGame')
 app.config['SECRET_KEY'] = 't(X9Day:V{nygE8+3Q36(9h#<)u7=i]U,X/?Xrd`)pt+BHR&x+d/HX9<k.l=rbS'
+
+db = sqlite3.connect('wordgame.db')
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/game')
+def game():
+    return render_template('game.html')
+
+
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+
+@app.route('/login')
+def profile():
+    return render_template('profile.html')
+
+
+@app.route('/signup')
+def profile():
+    return render_template('profile.html')
+
+
+@app.route('/logout')
+def profile():
+    return render_template('profile.html')
 
 
 if __name__ == '__main__':
--- dbinit.py
+++ dbinit.py
@@ -1,17 +1,19 @@
 import sqlite3
 
-db = sqlite3.connect('words.db')
+db = sqlite3.connect('wordgame.db')
 try:
 
     with db:
-        db.execute("""DROP TABLE IF EXISTS words""")
         db.execute("""CREATE TABLE IF NOT EXISTS words (
                         word        TEXT UNIQUE PRIMARY KEY NOT NULL,
                         word_len    INTEGER NOT NULL, 
                         last_used   DATE
                     )""")
-        db.execute("""CREATE INDEX word_len_index ON words (word_len)""")
-        db.execute("""CREATE INDEX last_used_index on words (last_used)""")
+        db.execute("""CREATE INDEX IF NOT EXISTS word_len_index ON words (word_len)""")
+        db.execute("""CREATE INDEX IF NOT EXISTS last_used_index on words (last_used)""")
 
     with db:
         db.execute("""""")
+
+finally:
+    db.close()
