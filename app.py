e58fac11f978b17957e397fd6bde05e0c94f05b1
Ron Smith <ron.smith@thataintworking.com>
2024-02-08 16:58:54 -0500

    login and index working

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, request, render_template, flash, session, url_for, redirect
-from impl import get_current_game, get_authenticated_user
+from impl import get_current_game, get_authenticated_user, create_user
 
 app = Flask('WordGame')
 app.config['SECRET_KEY'] = 't(X9Day:V{nygE8+3Q36(9h#<)u7=i]U,X/?Xrd`)pt+BHR&x+d/HX9<k.l=rbS'
 
+
+@app.after_request
+def after_request(resp):
+    # resp.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate, public, max-age=0'
+    resp.headers['Cache-Control'] = 'no-store, max-age=0'
+    resp.headers['Pragma'] = 'no-cache'
+    resp.headers['Expires'] = '0'
+    return resp
 
 @app.route('/')
 def index():
@@ -25,9 +33,9 @@ def profile():
 @app.route('/login', methods=['GET','POST'])
 def login():
     if request.method == 'POST':
+        session.clear()
         user = get_authenticated_user(request.form['email'], request.form['password'])
         if user:
-            session.new()
             session['user'] = user
             return redirect(url_for(request.args.get('next', 'index')))
         else:
@@ -35,14 +43,23 @@ def login():
     return render_template('login.html')
 
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        session.clear()
+        msg, category = create_user(
+            request.form['email'], request.form['name'], request.form['password'], request.form['confirm'])
+        if not msg:
+            return redirect(url_for('index'))
+        else:
+            flash(msg, category)
     return render_template('signup.html')
 
 
 @app.route('/logout')
 def logout():
-    return "Logout"
+    session.clear()
+    return redirect(url_for('index'))
 
 
 @app.route('/forgot-password')
--- impl.py
+++ impl.py
@@ -1,9 +1,7 @@
 import sqlite3
 import threading
 import functools
-from werkzeug.security import generate_password_hash
-
-db = sqlite3.connect('wordgame.db')
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 def synchronized(wrapped):
@@ -19,29 +17,46 @@ def synchronized(wrapped):
 @synchronized
 def get_current_game():
     """gets the current game or creates a new one if a game for today doesn't exist"""
-    cur = db.execute("""SELECT id, game_date FROM games WHERE game_date == date()""")
+    db = sqlite3.connect('wordgame.db')
+    cur = db.execute("""SELECT id, game_date FROM games WHERE game_date == date('now')""")
     row = cur.fetchone()
     if not row:
-        pass  # TODO: create a new game
-        # remember to select the game data again after creating it
+        cur = db.execute("""SELECT word FROM words
+            WHERE word_len = 5
+              AND (last_used IS NULL OR last_used < date('now', '-1 years'))
+            ORDER BY random()
+            LIMIT 1
+            """)
+        row = cur.fetchone()
+        word = row[0]
+        with db:
+            db.execute("""INSERT INTO games (word, game_date) VALUES (?, date('now'))""", (word,))
+            db.execute("""UPDATE words SET last_used = date('now') WHERE word = ?""", (word,))
+        cur = db.execute("""SELECT id, game_date FROM games WHERE game_date == date('now')""")
+        row = cur.fetchone()
 
     return {'id': row[0], 'game_date': row[1]}
 
 
 def get_authenticated_user(email, password):
+    db = sqlite3.connect('wordgame.db')
     cur = db.execute("""
-        SELECT users.id, users.email, users.name, games.game_date as last_play
+        SELECT users.id, users.email, users.name, games.game_date as last_play, users.pw_hash
             FROM users, games, plays
             WHERE users.email = ?
-              AND users.pw_hash = ?
               AND users.active = TRUE
               AND plays.user_id = users.id
               AND plays.game_id = games.id
             ORDER BY games.game_date DESC
-            LIMIT 1""",
-                (email, generate_password_hash(password)))
+            LIMIT 1""", (email,))
     row = cur.fetchone()
-    if row:
-        return {'id': row[0], 'email': row[1], 'name': row[2], 'game_date': row[3]}
+    if row and check_password_hash(row[4], password):
+        return {'id': row[0], 'email': row[1], 'name': row[2], 'last_play': row[3]}
     else:
         return None
+
+
+def create_user(email, name, password, confirm):
+    """return ('success','success') or success or (msg, category) on fail/error"""
+    pass  # TODO
+
