aaa0c5b7a05d07ce50ac8c4f11d48836b508e9a9
Ron Smith <ron.smith@thataintworking.com>
2025-06-11 20:40:58 -0400

    reformatted SQL

--- play.py
+++ play.py
@@ -25,15 +25,16 @@ def get_current_game(include_word=False):
         cur = db.execute("""SELECT id, game_date, word FROM games WHERE game_date == date('now', ?)""", (tz_adj,))
         row = cur.fetchone()
         if not row:
-            cur = db.execute("""SELECT word FROM words
+            cur = db.execute("""
+                SELECT word FROM words
                 WHERE word_len = 5
                   AND (last_used IS NULL OR last_used < date('now', ?, '-1 years'))
                 ORDER BY random()
                 LIMIT 1
-                """, (tz_adj,))
+            """, (tz_adj,))
             row = cur.fetchone()
             word = row[0]
-            with db:
+            with db:  # creates a trnsaction balance
                 db.execute("""INSERT INTO games (word, game_date) VALUES (?, date('now', ?))""", (word, tz_adj))
                 db.execute("""UPDATE words SET last_used = date('now', ?) WHERE word = ?""", (tz_adj, word,))
             cur = db.execute("""SELECT id, game_date, word FROM games WHERE game_date == date('now', ?)""", (tz_adj,))
@@ -53,43 +54,58 @@ def get_scoreboard_data():
     scoreboard = []
     try:
         cur = db.execute("""
-            SELECT users.name, today.attempts, today.win, yesterday.attempts, yesterday.win,
-                   (SELECT julianday('now', :tzadj) - julianday(max(date(timestamp)))
-                    FROM attempts WHERE user_id = users.id) "last_played",
-                   coalesce(avg_attempts, 0), coalesce(elo_rank, 0)
-            FROM users
-            LEFT OUTER JOIN (
-                SELECT user_id, date(timestamp) "date", count(*) "attempts", max(success) "win"
-                FROM attempts
-                WHERE date = date('now', :tzadj)
-                GROUP BY user_id, date
-            ) as today ON today.user_id = users.id
-            LEFT OUTER JOIN (
-                SELECT user_id, date(timestamp) "date", count(*) "attempts", max(success) "win"
-                FROM attempts
-                WHERE date = date('now', '-1 day', :tzadj)
-                GROUP BY user_id, date
-            ) as yesterday ON yesterday.user_id = users.id
-            LEFT OUTER JOIN (
-                WITH scores as (
-                    SELECT user_id, date(timestamp) "date", count(*) "attempts", max(success) "win"
-                    FROM attempts
-                    GROUP BY user_id, date
-                )
-                SELECT user_id, date, avg_attempts, (wins + 5 * all_win_avg) / (wins + losses + 5) as elo_rank
-                FROM (
-                    SELECT user_id, date, avg(attempts) as avg_attempts,
-                        (select count(*) from scores w where w.user_id = s.user_id and win == TRUE)  as wins,
-                        (select count(*) from scores l where l.user_id = s.user_id and win == FALSE) as losses,
-                        (select avg(win) from scores) as all_win_avg
-                    FROM scores s
-                    GROUP BY user_id
-                )
-                ORDER BY elo_rank
-            ) as averages ON averages.user_id = users.id
-            WHERE users.active = True
-            ORDER BY elo_rank DESC, avg_attempts ASC 
-        """, {'tzadj': tz_adj})
+                         SELECT users.name,
+                                today.attempts,
+                                today.win,
+                                yesterday.attempts,
+                                yesterday.win,
+                                (SELECT julianday('now', :tzadj) - julianday(max(date(timestamp)))
+                                 FROM attempts
+                                 WHERE user_id = users.id) "last_played",
+                                coalesce(avg_attempts, 0),
+                                coalesce(elo_rank, 0)
+                         FROM users
+                                  LEFT OUTER JOIN (SELECT user_id,
+                                                          date(timestamp) "date",
+                                                          count(*)        "attempts",
+                                                          max(success)    "win"
+                                                   FROM attempts
+                                                   WHERE date = date('now', :tzadj)
+                                                   GROUP BY user_id, date) as today ON today.user_id = users.id
+                                  LEFT OUTER JOIN (SELECT user_id,
+                                                          date(timestamp) "date",
+                                                          count(*)        "attempts",
+                                                          max(success)    "win"
+                                                   FROM attempts
+                                                   WHERE date = date('now', '-1 day', :tzadj)
+                                                   GROUP BY user_id, date) as yesterday ON yesterday.user_id = users.id
+                                  LEFT OUTER JOIN (WITH scores as (SELECT user_id,
+                                                                          date(timestamp) "date",
+                                                                          count(*)        "attempts",
+                                                                          max(success)    "win"
+                                                                   FROM attempts
+                                                                   GROUP BY user_id, date)
+                                                   SELECT user_id,
+                                                          date,
+                                                          avg_attempts,
+                                                          (wins + 5 * all_win_avg) / (wins + losses + 5) as elo_rank
+                                                   FROM (SELECT user_id,
+                                                                date,
+                                                                avg(attempts)                                  as avg_attempts,
+                                                                (select count(*)
+                                                                 from scores w
+                                                                 where w.user_id = s.user_id and win == TRUE)  as wins,
+                                                                (select count(*)
+                                                                 from scores l
+                                                                 where l.user_id = s.user_id
+                                                                   and win == FALSE)                           as losses,
+                                                                (select avg(win) from scores)                  as all_win_avg
+                                                         FROM scores s
+                                                         GROUP BY user_id)
+                                                   ORDER BY elo_rank) as averages ON averages.user_id = users.id
+                         WHERE users.active = True
+                         ORDER BY elo_rank DESC, avg_attempts ASC
+                         """, {'tzadj': tz_adj})
 
         for player, today_attempts, today_win, yesterday_attempts, yesterday_win, last_played, avg_attempts, elo_rank in cur:
 
@@ -127,13 +143,15 @@ def get_scoreboard_data():
 def get_last_play_data(user):
     db = get_db()
     try:
-        cur = db.execute("""SELECT game_date, count(*) "attempts", max(success) "win"
-                                FROM games, attempts
-                                WHERE games.id = attempts.game_id
-                                  AND attempts.user_id = ?
-                                GROUP BY game_date
-                                ORDER BY game_date desc
-                                LIMIT 1""", (user['id'],))
+        cur = db.execute("""
+            SELECT game_date, count(*) "attempts", max(success) "win"
+            FROM games, attempts
+            WHERE games.id = attempts.game_id
+              AND attempts.user_id = ?
+            GROUP BY game_date
+            ORDER BY game_date desc
+            LIMIT 1
+        """, (user['id'],))
         row = cur.fetchone()
         if not row:
             return None
@@ -159,23 +177,30 @@ def do_submit_word(user, word):
             return {'status': 'error', 'message': 'Unknown word. Try another.'}
 
         with db:
-            db.execute("""INSERT INTO attempts (user_id, game_id, word, timestamp, success)
-                            VALUES (?, ?, ?, datetime('now', ?), ?)""",
-                       (user['id'], game['id'], word, get_tz_adj(), (word == game['word'])))
-
-        return generate_game_state(user)
+            db.execute("""
+                INSERT INTO attempts (user_id, game_id, word, timestamp, success)
+                VALUES (:userid, :gameid, :word, datetime('now', :tzadj), :success)
+            """, {
+                'userid': user['id'],
+                'gameid': game['id'],
+                'word': word,
+                'txadj': get_tz_adj(),
+                'success': (word == game['word'])
+            })
     finally:
         db.close()
+    return generate_game_state(user)
 
 
 def generate_game_state(user):
     game = get_current_game(include_word=True)
     db = get_db()
     try:
-        cur = db.execute("""SELECT word, success FROM attempts 
-                            WHERE user_id = ? AND game_id = ? 
-                            ORDER BY timestamp ASC""",
-                         (user['id'], game['id']))
+        cur = db.execute("""
+            SELECT word, success FROM attempts 
+            WHERE user_id = ? AND game_id = ? 
+            ORDER BY timestamp ASC
+        """, (user['id'], game['id']))
         status = 'playing'
         rows = []
         kb_green = set()
