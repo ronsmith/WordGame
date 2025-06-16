0c914cc6ea6ea21e0cbeb5f5347fcee5d7604a32
Ron Smith <ronsmith@users.noreply.github.com>
2025-06-16 00:11:23 -0400

    Added test script

---
+++ test.py
@@ -0,0 +1,11 @@
+from unittest import TestCase, main
+from play import game_state
+
+class TestGameState(TestCase):
+    
+    def test_easy_word(self):
+        pass # TODO 
+
+
+if __name__ == '__main__':
+    main()
