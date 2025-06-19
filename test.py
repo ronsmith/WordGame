77f650553a449bd2d8657291ab9d076c739c9ce3
Ron Smith <ronsmith@users.noreply.github.com>
2025-06-18 23:55:14 -0400

    writing tests WIP

--- test.py
+++ test.py
@@ -4,15 +4,24 @@ from play import game_state
 
 def _test(word, guess_response_list):
     attempts = []
+    responses = []
     for guess, response in guess_response_list:
         attempts.append((guess, guess == word))
         state = game_state(attempts, word)
+        responses.append(response)
+        # TODO
 
 
 class TestGameState(TestCase):
     
     def test_easy_word(self):
-        word = "BANJO"
+        _test('BANJO', (
+            ('PIANO', 'BBYYG'),
+            ('ARNXO', 'YBGBG'),
+            ('FANDO', 'BGGBG'),
+            ('JANBO', 'YGGYG'),
+            ('BANJO', 'GGGGG')
+        ))
 
 
 
